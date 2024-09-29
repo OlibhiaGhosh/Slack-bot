0009e71679c99c8c642eeb3b93304d6e51b3a6ab
Olibhia Ghosh <olibhiag@gmail.com>
2024-09-29 16:18:27 +0530

    added some more features

--- app.js
+++ app.js
@@ -11,19 +11,49 @@ const app = new App({
   await app.start(process.env.PORT || port);
   console.log("Bolt app started!!");
 })();
-app.message("hey", async ({ command, say }) => {
+app.message("hey", async ({ message, say }) => {
     try {
-      say("Hello Human!");
+      say(`Hello <@${message.user}>!`);
     } catch (error) {
         console.log("err")
       console.error(error);
     }
 });
-app.message(/hi/, async ({ command, say }) => {
+app.message(/hi/, async ({ message , say }) => {
     try {
-      say("Hello Human!");
+      say(`Hello <@${message.user}>!`);
     } catch (error) {
         console.log("err")
       console.error(error);
     }
 });
+app.message(`knock knock`, async({message , say}) => {
+  await say(`_Who's there?_`);
+})
+// This will match any message that contains 👋
+app.message(':wave:', async ({ message, say }) => {
+  // Handle only newly posted messages here
+  if (message.subtype === undefined
+    || message.subtype === 'bot_message'
+    || message.subtype === 'file_share'
+    || message.subtype === 'thread_broadcast') {
+    await say(`Hello, <@${message.user}>`);
+  }
+});
+app.message(/description/, async ({ message , say }) => {
+  try {
+    say(`<@${message.user}> is a full stack developer!`);
+  } catch (error) {
+      console.log("err")
+    console.error(error);
+  }
+});
+app.message(/bye/, async ({ message , say }) => {
+  try {
+    say(` Goodbye <@${message.user}>! Have a good day`);
+  } catch (error) {
+      console.log("err")
+    console.error(error);
+  }
+});
+
